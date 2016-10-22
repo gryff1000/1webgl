8ef0256ab475ebdbbf979052d22a510822b3d25e
gryff1000 <gryff1000@users.noreply.github.com>
2016-10-22 16:07:52 -0400

    Update createScene.js

--- mfb/createScene.js
+++ mfb/createScene.js
@@ -32,6 +32,8 @@ function createWorld(){
 			if( isMobile.any() ){ 
 				var myLight = myScene.getLightByName("Hemi.top");
 				myLight.intensity = 2;
+				var myWall = myScene.getMaterialByName("dk14_34b.blue");
+				myWall.diffuseColor = new BABYLON.Color3(0.125,.125,.25);
 			}
 		
 			// create additional cameras from blender
