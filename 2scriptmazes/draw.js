26da1e0bbe68d8ba163b54651e741eb6213ff57f
gryff1000 <gryff1000@users.noreply.github.com>
2023-09-16 14:13:11 -0400

    Add files via upload

---
+++ 2scriptmazes/draw.js
@@ -0,0 +1,500 @@
+
+//theMaze = drawMaze(xSize,ySize,0,theCells,myScene);
+function drawMaze(x,y,num,theCells,myScene) {
+
+//var harch = myScene.getMeshByName("wallh_text");
+
+	var hwall = myScene.getMeshByName("wallh_text");
+	//hwall.setEnabled(false);
+	var vwall = myScene.getMeshByName("wallv_text");
+	//vwall.setEnabled(false);
+	var varch = myScene.getMeshByName("arch_v");
+	//varch.setEnabled(false);
+	var harch = myScene.getMeshByName("arch_h");
+	
+	var theHdoor = myScene.getMeshByName("hdoor");
+	var theVdoor = myScene.getMeshByName("vdoor");
+	
+	
+	// delete this later
+	var count =0;
+
+	
+// place doors to stairs in the multi array  of cells by switching certain values to 1 for each maze
+	switch (num) {
+	
+		case 0: // maze1 - left bottom only one door
+			var aCell = theCells[lastrow][lastcol];
+			aCell[1] = 2;
+			//var port = agate.createInstance("agate_" + num);
+			//port.position = new BABYLON.Vector3(-4, 0, -22);
+			//port.rotation.y = Math.PI/2;
+			aCell[2] = 0;
+			break;
+			
+		case 1:// maze 2 - bottom right
+		
+			var aCell = theCells[lastrow][firstcol];
+			aCell[3] = 2;
+			var aCell = theCells[firstrow][lastcol];
+			aCell[0] = 2;
+			break;
+			
+		case 2: // maze - top right
+			
+			var aCell = theCells[firstrow][firstcol];
+			aCell[3] = 2;
+			var aCell = theCells[lastrow][lastcol];
+			aCell[2] = 2;
+			break;
+			
+		case 3: // maze top left - only one door
+		
+			var aCell = theCells[firstrow][lastcol];
+			aCell[1] = 2;
+			var aCell = theCells[lastrow][firstcol];
+			aCell[2] = 2;
+			break;
+			
+		default:
+			break;
+	
+	};
+	
+	var bwCount = 0;
+	var lCount = 0;
+
+					//draw the cell walls
+					// outer loop - go down columns
+					//console.log("last cell = " + theCells[lastrow][lastcol]);
+					//console.log("first cell = " + theCells[0][0]);
+					for (i = 0; i < ySize; i++){
+					
+						var vPosz  = cent_zero_y - i*4
+						//console.log(vPosz);
+						//then inner loop - go across columns for each row
+						for (j= 0; j < xSize; j++) {
+							var hPosz = cent_zero_x + j*4;
+							// get each cell from the cell array
+							var aCell = theCells[i][j];
+							//console.log(aCell.length);
+							
+							// now draw the 4 walls for each cell 0 = top, 1 = right, 2 = bottom, 3 = left
+							for (k =0; k < aCell.length; k++){
+							
+								switch (k) {
+								
+								case 0:
+									
+									if(aCell[k] == 0){ // create solid horizontal wall at top of cell
+										var newHWall = hwall.createInstance("hwall_" +num + "_" + i + j);
+										//var newHWall = harch.createInstance("harch_" +num + "_" + i + j);
+										newHWall.position = new BABYLON.Vector3(hPosz, 0, vPosz+2);
+										//count = count + 1;
+										//console.log(count);
+									}
+									
+									else if ((aCell[k] == 1)){ 	// else create a horizontal arch at top of cell
+										var newHWall = harch.createInstance("harch_" +num + "_" + i + j);
+										newHWall.position = new BABYLON.Vector3(hPosz, 0, vPosz+2);
+										//theHdoor.position = new BABYLON.Vector3(-hPosz, 0, -vPosz+2);
+										//console.log(newHWall.name + " = " + theHdoor.position);
+									}
+									else {
+										
+										//var newHWall = harch.createInstance("harch_" +num + "_" + i + j);
+										//newHWall.position = new BABYLON.Vector3(hPosz, 0, vPosz+2);
+										//theHdoor.position = new BABYLON.Vector3(hPosz, 0, vPosz+2);
+										//console.log("Pos = " + theHdoor.position);
+										//var newHWall = hparch.createInstance("hparch_" +num + "_" + i + j);
+										//newHWall.position = new BABYLON.Vector3(hPosz, 0, vPosz+2);
+										//var newHLock = hlock.createInstance("hlock_" +num + "_" + i + j);
+										//newHLock.parent = newHWall;
+										
+										
+									}
+									
+									
+									newHWall.checkCollisions =true;
+									newHWall.parent = ground[num];	
+									break;
+									
+								case 1:
+									
+									if (aCell[k] == 0){ // create a solid vertical wall at right of cell
+										var newVWall = vwall.createInstance("vwall_" +num + "_" + i + j);
+										newVWall.position = new BABYLON.Vector3(hPosz+2, 0, vPosz);
+										
+										count = count + 1;
+									}
+									else if (aCell[k] == 1){	// create a vertical arch
+										var newVWall = varch.createInstance("varch_" +num + "_" + i + j);
+										newVWall.position = new BABYLON.Vector3(hPosz+2, 0, vPosz);
+										//theVdoor.position = new BABYLON.Vector3(hPosz+2, 0, vPosz);
+									}
+									else {
+										
+										var newHWall = varch.createInstance("varch_" +num + "_" + i + j);
+										newHWall.position = new BABYLON.Vector3(hPosz+2, 0, vPosz);
+										theVdoor.position = new BABYLON.Vector3(hPosz+2, 0, vPosz);
+										//newHWall.position = new BABYLON.Vector3(10, 4, 10);
+										
+										
+										//var newVWall = vparch.createInstance("vparch_" +num + "_" + i + j);
+										//newVWall.position = new BABYLON.Vector3(hPosz+2, 0, vPosz);
+										//var newVLock = vlock.createInstance("vlock_" +num + "_" + i + j);
+										//newVLock.parent = newVWall;
+										//var newVLever = lever.createInstance("lever_" +num + "_" + i + j);
+										//newVLever.parent = newVWall;
+										//var newVGate = drawGates(x, y, num, newVWall, myScene);
+										
+										//newVGate[0].parent = newVWall;
+										//newVGate[1].parent = newVWall;
+									}
+									newVWall.checkCollisions =true;
+									newVWall.parent = ground[num];
+									break;
+									
+								case 2: 
+								if (i == lastrow) {	// only draw bottom wall if it is the last row
+									if (aCell[k] == 0){ //create a solid horizontal wall at bottom of cell
+											var newHWall = hwall.createInstance("hwall_" +num + "_" + i + j);
+											newHWall.position = new BABYLON.Vector3(hPosz, 0, vPosz-2);
+											count = count + 1;
+											//console.log(count);
+									} 
+									else if (aCell[k] == 1){	//create a solid horizontal arch at bottom of cell
+										var newHWall = harch.createInstance("harch_" +num + "_" + i + j);
+										newHWall.position = new BABYLON.Vector3(hPosz, 0, vPosz-2);
+										//newHWall.parent = ground[num];
+									}
+									else {
+										
+										//var newHWall = varch.createInstance("varch_" +num + "_" + i + j);
+										//newHWall.position = new BABYLON.Vector3(hPosz, 0, vPosz-2);
+										//newHWall.position = new BABYLON.Vector3(10, 4, 10);
+										
+										
+										//var newVWall = vparch.createInstance("vparch_" +num + "_" + i + j);
+										//newVWall.position = new BABYLON.Vector3(hPosz, 4, vPosz-14);
+										//var newVLock = vlock.createInstance("vlock_" +num + "_" + i + j);
+										//newVLock.parent = newVWall;
+										//var newVLever = lever.createInstance("lever_" +num + "_" + i + j);
+										//newVLever.parent = newVWall;
+										//var newVGate = drawGates(x, y, num, newVWall, myScene);
+										
+										//newVGate[0].parent = newVWall;
+										//newVGate[1].parent = newVWall;
+									}
+									newHWall.checkCollisions =true;
+									newHWall.parent = ground[num];
+									//bwCount = bwCount + 1;
+									//console.log(bwCount);
+								}
+									break;
+								case 3:
+								if (j == firstrow) {	// only draw left wall if it is the first column	
+									if(aCell[k] == 0){	// create a solid vertical wall at left of cell
+										var newVWall = vwall.createInstance("vwall_" +num + "_" + i + j);
+										newVWall.position = new BABYLON.Vector3(hPosz-2, 0, vPosz);
+										count = count + 1;
+									}
+									else if (aCell[k] == 1)	{// create a solid vertical arch at left of cell
+										var newVWall = varch.createInstance("varch_" +num + "_" + i + j);
+										newVWall.position = new BABYLON.Vector3(hPosz-2, 0, vPosz);
+									}
+									else{
+										//var newVWall = vparch.createInstance("vparch_" +num + "_" + i + j);
+										//newVWall.position = new BABYLON.Vector3(hPosz-2, 0, vPosz);
+										
+									
+									}
+									newVWall.checkCollisions =true;
+									newVWall.parent = ground[num];
+									lCount = lCount + 1;
+									
+								}
+									break;	
+								
+								default:
+								
+									break;
+								
+								};
+							}; //end k loop
+							
+						}; // end j loop
+						
+					}; // end i loop
+					
+					theHdoor.position = new BABYLON.Vector3(-hPosz, 0, -vPosz+2);
+					//console.log("HDoor pos = " + theHdoor.position);
+					
+					
+					// move the maze into position
+					//var xPosition = xSize*2 + 8;
+					//var zPosition = xSize*2 + 8;
+					vwall.setEnabled(false);
+					hwall.setEnabled(false);
+					varch.setEnabled(false);
+					harch.setEnabled(false);
+					
+					
+					
+					//add y position if maze is not square
+					
+					
+			/*		if(num ==0) ground[num].position = new BABYLON.Vector3(-xPosition, 4*num, -zPosition);
+					
+					if(num ==1) ground[num].position = new BABYLON.Vector3(xPosition, 4*num, -zPosition);
+					if(num ==2) ground[num].position = new BABYLON.Vector3(xPosition, 4*num, zPosition);
+					if(num ==3) ground[num].position = new BABYLON.Vector3(-xPosition, 4*num, zPosition);
+};					//end draw the cell walls
+*/
+};
+
+
+
+/*
+function drawStairs (x,y,num,myScene) {
+	var rotY = (Math.PI/2)*num
+	var ramp = myScene.getMeshByName("newslide");
+	ramp.setEnabled(false);
+	ramp.isVisible = true;
+	var aRamp = ramp.createInstance("ramp" + num);
+	aRamp.checkCollisions = true;
+	aRamp.isVisible = false;
+	var wall_st = myScene.getMeshByName("st_wall");
+	wall_st.setEnabled(false);
+	var aWall = wall_st.createInstance("wall_st" + num);
+	aWall.checkCollisions = true;
+	var steps = myScene.getMeshByName("stairs");
+	steps.setEnabled(false);
+	var theSteps = steps.createInstance("theSteps" + num);
+	theSteps.checkCollisions = false;
+	theSteps.isVisible = true;
+	var arch = myScene.getMeshByName("arch_vp");
+	arch.setEnabled(false);
+	anArch1 = arch.createInstance("arch1" + num);
+	anArch1.position = new BABYLON.Vector3(-8,0, 0);
+	anArch1.checkCollisions = true;
+	anArch2 = arch.createInstance("arch2" +num );
+	anArch2.checkCollisions = true;
+	anArch2.position = new BABYLON.Vector3(8,4, 0);
+	var aVGate = myScene.getMeshByName("gate_v");
+	aVGate.setEnabled(false); 
+	var theVGate1 = aVGate.createInstance("theVGate1" +num);
+	theVGate1.checkCollisions = true;
+	var theVGate2 = aVGate.createInstance("theVGate2" +num);
+	theVGate2.checkCollisions = true;
+	var locks = myScene.getMeshByName("lock_v");
+	locks.setEnabled(false);
+	var aLock = locks.createInstance("lock1" +num );
+	
+	var aVLever = myScene.getMeshByName("lever");
+	aVLever.setEnabled(false); 
+	var theVLever = aVLever.createInstance("theVLever" + num);
+	
+	//aLock.scaling = new BABYLON.Vector3(4,4,4);
+	theVLever.parent = anArch1;
+	aLock.parent = anArch1;
+	theVGate1.parent = anArch1;
+	theVGate2.parent = anArch2;
+	aWall.parent = aRamp;
+	theSteps.parent = aRamp;
+	anArch1.parent = aRamp;
+	anArch2.parent = aRamp;
+	
+	switch (num) {
+	
+		case 0:
+			aRamp.rotation.y = rotY;
+			aRamp.position = new BABYLON.Vector3(0, 0, (-(x+2)*4 + 2));
+			break;
+			
+		case 1:
+			aRamp.rotation.y = -rotY ;
+			aRamp.position = new BABYLON.Vector3(((x+2)*4 - 2), 4, 0);
+			break;
+		
+		case 2:
+			aRamp.rotation.y = rotY ;
+			aRamp.position = new BABYLON.Vector3(0, 8, ((x+2)*4 - 2));
+			break;
+			
+		case 3:
+			aRamp.rotation.y = -rotY ;
+			aRamp.position = new BABYLON.Vector3(-((x+2)*4 - 2), 12, 0);	
+		break;
+		
+		default:
+			break;
+			
+	}	
+
+// On pick interpolations to open/close gate
+	var control = function(mesh1, mesh2, mesh3) {
+							
+	// events done in sequence 
+		mesh1.actionManager = new BABYLON.ActionManager(myScene);
+				
+	// gate action
+		mesh1.actionManager.registerAction(new BABYLON.InterpolateValueAction(BABYLON.ActionManager.OnPickTrigger, mesh2, "position.y", +2.3, 2500))
+		.then(new BABYLON.InterpolateValueAction(BABYLON.ActionManager.OnPickTrigger, mesh2, "position.y", 0.0, 2500))
+		mesh1.actionManager.registerAction(new BABYLON.InterpolateValueAction(BABYLON.ActionManager.OnPickTrigger, mesh3, "position.y", +0.0, 250))
+		.then(new BABYLON.InterpolateValueAction(BABYLON.ActionManager.OnPickTrigger, mesh3, "position.y", 2.3, 2500))
+	//lever action
+		mesh1.actionManager.registerAction(new BABYLON.InterpolateValueAction(BABYLON.ActionManager.OnPickTrigger, mesh1, "rotation.z", Math.PI/12, 500))
+		.then(new BABYLON.InterpolateValueAction(BABYLON.ActionManager.OnPickTrigger, mesh1, "rotation.z", 0, 250));
+		mesh1.actionManager.registerAction(new BABYLON.ExecuteCodeAction(BABYLON.ActionManager.OnPickTrigger, playGate));
+					
+		}
+		
+		control(theVLever, theVGate1, theVGate2);
+		
+
+	
+	
+//get all the tower parts and move them to top of last staircase
+	var theTower = myScene.getMeshByName("theTower");
+	theTower.position = new BABYLON.Vector3(-((x+2)*4 - 2), 16, -8);
+	
+	var towerDoor = myScene.getMeshByName("fdoor1");
+	var dknob = myScene.getMeshByName("dknob");
+	var outside = myScene.getMeshByName("outside");
+	var aLight = myScene.getLightByName("Hemi");
+	aLight.includedOnlyMeshes = [outside];
+	aLight.setEnabled(true); 
+	
+
+// On pick interpolations to open door
+	var openDoor = function(mesh1,mesh2) {
+			mesh1.actionManager = new BABYLON.ActionManager(myScene);
+			//knob action
+			mesh1.actionManager.registerAction(new BABYLON.InterpolateValueAction(BABYLON.ActionManager.OnPickTrigger, mesh1, "position.x", -0.6000, 250))
+			// gate action
+			mesh1.actionManager.registerAction(new BABYLON.InterpolateValueAction(BABYLON.ActionManager.OnPickTrigger, mesh2, "rotation.y", -Math.PI/4, 2500))
+			//play sound	
+			mesh1.actionManager.registerAction(new BABYLON.ExecuteCodeAction(BABYLON.ActionManager.OnPickTrigger, playDoor));
+		};
+		
+	openDoor(dknob,towerDoor);
+	
+	
+	return; //end door creation
+}
+*/
+
+/*
+function drawHound(name,  scene) {
+	BABYLON.SceneLoader.ImportMesh(null, "", "hound1c_rigged1c.babylon", myScene, function (newMeshes, particleSystems, skeletons) {
+		var theDog = myScene.getMeshByName("hound");
+		theDog.setEnabled(false);
+		//var teeth1 = myScene.getMeshByName("teeth_back.L1");
+		//var teeth2 = myScene.getMeshByName("teeth.L1");
+		//var eyes = myScene.getMeshByName("eyes");
+		if(name> 0){
+			
+			var myDog = theDog.clone("theDog"+name);
+
+			//var myEyes = eyes.clone("eyes"+name);
+			//var myTeeth1 = teeth1.clone("teeth1"+name);
+			//var myTeeth2 = teeth2.clone("teeth2"+name);
+
+			//myTeeth1.parent = myDog;
+			//myTeeth2.parent = myDog;
+			//myEyes.parent = myDog;
+			
+			var dogLight = new BABYLON.PointLight("dogLight", new BABYLON.Vector3(0, 0, 0), myScene);
+			dogLight.parent = myDog;
+			dogLight.position.y = .8;
+			dogLight.position.z = -.5;
+			
+			dogLight.diffuse = new BABYLON.Color3(0, 1, 0);
+			dogLight.specular = new BABYLON.Color3(0, .6, 0);
+			dogLight.range = 2;
+			dogLight.intensity = 2;
+			dogLight.includedOnlyMeshes = [myDog];
+			
+			myDog.scaling = new BABYLON.Vector3(0.85, 0.85, 0.85);
+			//myDog.scaling = new BABYLON.Vector3(1.75, 1.75, 1.75);
+			myDog.position.x = -.5;
+			myDog.position.z = 199.4; //new BABYLON.Vector3(3, -2, 0);
+			//myDog.rotation.y = Math.PI/2;
+
+			//myDog.rotation.y = dRotations[name-1]*Math.PI/4;
+			
+		}
+		else {
+		
+		//theDog.setEnabled(false);
+		
+		}
+	
+	});
+}	
+*/
+
+
+
+
+
+
+
+
+
+/*
+function drawGates(x,y,num,whichWall, myScene) {
+		tempcount = tempcount + 1;
+		console.log(tempcount);
+		var aVGate = myScene.getMeshByName("gate_v");
+		aVGate.setEnabled(false); 
+		var theVGate = aVGate.createInstance("theVGate" +num);
+		
+		theVGate.parent = whichWall;
+		var aVLever = myScene.getMeshByName("lever");
+		aVLever.setEnabled(false); 
+		var theVLever = aVLever.createInstance("theVLever" + num);
+
+		theVLever.parent = whichWall;
+		
+		//if(tempcount == 2){
+		//theVGate.scaling = new BABYLON.Vector3(10,10,10);
+		//theVLever.scaling = new BABYLON.Vector3(10,10,10);
+		//}
+
+// On pick interpolations to open/close gate
+		var control = function(mesh1, mesh2) {
+							
+			// events done in sequence 1st click prints object data, 2nd click sets wireframe value
+			mesh1.actionManager = new BABYLON.ActionManager(myScene);
+				
+			// gate action
+			mesh1.actionManager.registerAction(new BABYLON.InterpolateValueAction(BABYLON.ActionManager.OnPickTrigger, mesh2, "position.y", +2.3, 2500))
+			.then(new BABYLON.InterpolateValueAction(BABYLON.ActionManager.OnPickTrigger, mesh2, "position.y", 0.1, 2500));
+			//lever action
+			mesh1.actionManager.registerAction(new BABYLON.InterpolateValueAction(BABYLON.ActionManager.OnPickTrigger, mesh1, "rotation.z", Math.PI/12, 500))
+			.then(new BABYLON.InterpolateValueAction(BABYLON.ActionManager.OnPickTrigger, mesh1, "rotation.z", 0, 500));
+			//mesh1.actionManager.registerAction(new BABYLON.ExecuteCodeAction(BABYLON.ActionManager.OnPickTrigger, playGate));
+					
+		}
+		
+		control(theVLever, theVGate);
+		//control(theLever2, theDoor2);
+
+
+//var aGate = [theVGate, theVLever];
+//return aGate;
+return;
+};
+*/
+
+
+
+
+
+
+
+
+
+
---
+++ 2scriptmazes/loader.js
@@ -0,0 +1,76 @@
+﻿// my window size = 1920 x 945
+
+
+// The babylon engine
+var engine;
+// The current scene
+var myScene;
+// The HTML canvas
+var canvas;
+
+var sceneNum = 1;
+//var theScenes = new Array("rift1anim6new.babylon", "script_maze01.babylon", "village6.babylon");
+
+//--------------------Scene 1 variables---------------------------------
+var xSize = 7;
+var ySize = 7;
+var theCells = [];
+//var myScene = new BABYLON.Scene(engine);
+var lastrow = ySize-1;
+var lastcol =xSize -1;
+var firstcol = 0;
+var firstrow = 0;
+var halfx = xSize - Math.floor(xSize*.5);
+var halfy = ySize - Math.floor(ySize*.5);
+var cent_zero_x = -xSize*2 + 2;
+var cent_zero_y = ySize*2 - 2;
+var ground = [];
+var groundNum = 0;
+var talk;
+var divFps;
+var divDCs;
+var divAllMeshes;
+var divVerts;
+var sceneInstru;
+
+var exitNum = 0;
+
+
+window.onload = function () {
+
+    canvas = document.getElementById("renderCanvas");
+	talk = document.getElementById("words");
+	
+	//delete this line for final- for testing only
+	//divFps = document.getElementById("fps");
+	
+    // Check support
+    if (!BABYLON.Engine.isSupported()) {
+        window.alert('Browser not supported');
+    } 
+	else {
+		divFps = document.getElementById("fps");
+		divDCs = document.getElementById("DCs");
+		divAllMeshs = document.getElementById("meshes");
+		divVerts = document.getElementById("verts");
+		
+        engine = new BABYLON.Engine(canvas, true);
+		myScene = new BABYLON.Scene(engine);
+		sceneInstru = new BABYLON.SceneInstrumentation(myScene);
+		
+		
+		
+		createMaze();
+		
+	}
+};
+		
+//------------------------actions-----------------------------------------------------------		
+
+    // Resize window
+		
+    window.addEventListener("resize", function () {
+        engine.resize();
+    });
+		
+
